c24c2b7de53cab8c14644200821a0c3a02835d38
vkap34873847 <industrygadha@gmail.com>
2024-12-21 01:23:45 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Vedansh Kapoor";
             $("#").attr("href", "assets/images/");
         }
         else {
